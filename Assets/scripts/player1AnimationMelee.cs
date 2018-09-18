5bad1dfdead4c2af51f1bc620390aff0e7a2a07e
Legendary888 <dennistodorov88@gmail.com>
2018-09-18 15:33:51 +0300

    post processing set up

---
+++ Assets/scripts/player1AnimationMelee.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player1AnimationMelee : MonoBehaviour
+{
+    public float controlSpeed;
+    public bool controlSword1;
+    public bool controlSword1Block;
+
+
+    Animator anim;
+
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+        anim.SetFloat("Speed", controlSpeed);
+        anim.SetBool("sword1", controlSword1);
+        anim.SetBool("sword1block", controlSword1Block);
+
+
+        Debug.Log("controlSpeed is " + controlSpeed);
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            controlSpeed++;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlSpeed--;
+
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            controlSpeed = 0.2f;
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            controlSpeed = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            controlSpeed = 0.4f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            controlSpeed = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            controlSpeed = 0.6f;
+
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            controlSpeed = 0;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            controlSword1 = true;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            controlSword1 = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            controlSword1 = false;
+            controlSword1Block = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            controlSword1Block = false;
+        }
+
+    }
+}
---
+++ Assets/scripts/player1Animations.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player1Animations : MonoBehaviour
+{
+    public float controlSpeed;
+  
+    Animator anim;
+    public Transform aimPosition;
+    public Transform standardPosition;
+    public Transform currentPosition;
+
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        
+        anim.SetFloat("Speed", controlSpeed);
+        Debug.Log("controlSpeed is " + controlSpeed);
+        
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            controlSpeed++;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlSpeed--;
+
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            controlSpeed = 0.2f;
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            controlSpeed = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            controlSpeed = 0.4f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            controlSpeed = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            controlSpeed = 0.6f;
+
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            controlSpeed = 0;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            controlSpeed = 0.8f;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            controlSpeed = 0;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            playerAim();
+            controlSpeed = 1;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            playerStopAim();
+            controlSpeed = 0;
+
+        }
+
+    }
+    public void playerAim()
+    {
+        transform.localPosition = aimPosition.localPosition;
+    }
+    public void playerStopAim()
+    {
+        transform.localPosition = standardPosition.localPosition;
+
+    }
+}
