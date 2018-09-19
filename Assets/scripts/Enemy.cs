6f8994452fc1f7bc15e3a85939ff41e59509dd67
Legendary888 <dennistodorov88@gmail.com>
2018-09-19 14:34:14 +0300

    updated level

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float health = 50f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void TakeDamage(float amoumt)
+    {
+        health -= amoumt;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripts/playerWeapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerWeapon : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+    float nextTimeToFire = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+
+        }
+
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+                if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+                if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+    }
+}
