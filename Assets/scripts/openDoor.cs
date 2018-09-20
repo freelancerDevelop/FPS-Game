d79571d103fdb80d1c0085c0228e3f6d11ac492f
Legendary888 <dennistodorov88@gmail.com>
2018-09-20 18:01:56 +0300

    level improved

---
+++ Assets/scripts/openDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour {
+
+    public GameObject door;
+    public Transform standardPosition;
+    public Transform openPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // door.SetActive(false);
+            transform.localPosition = openPosition.position;
+            Debug.Log("door open");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //  door.SetActive(true);
+            transform.localPosition = standardPosition.position;
+            Debug.Log("door closed");
+
+        }
+    }
+}
