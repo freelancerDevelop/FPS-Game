7a0effc46ecb092240b3972cf7d6246e15258c89
Legendary888 <dennistodorov88@gmail.com>
2018-09-21 00:23:30 +0300

    enemies added

---
+++ Assets/scripts/player1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class player1 : MonoBehaviour {
+    public float playerHealth;
+    public float maxPlayerHealth;
+    public float playerEnergy;
+    public float maxPlayerEnergy;
+    public Slider playerHealthSlider;
+    public Slider playerEnergySlider;
+
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerHealthSlider.maxValue = maxPlayerHealth;
+        playerHealthSlider.value = playerHealth;
+
+        playerEnergySlider.maxValue = maxPlayerEnergy;
+        playerEnergySlider.value = playerEnergy;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            playerHealth -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            playerHealth += 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            playerEnergy -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            playerEnergy += 10;
+        }
+
+    }
+}
