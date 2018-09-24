7962a2882b133c2450d5c218aa5b421dc74531a6
Legendary888 <dennistodorov88@gmail.com>
2018-09-24 22:50:42 +0300

    inventory feature added

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Enemy : MonoBehaviour {
 
     public float health = 50f;
+    public Slider healthSlider;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        healthSlider.maxValue = health;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        healthSlider.value = health;
 	}
     public void TakeDamage(float amoumt)
     {
