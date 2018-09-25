1a4a177726a63e6cd45fae76bbf9372e0b3c701c
Legendary888 <dennistodorov88@gmail.com>
2018-09-26 00:44:59 +0300

    enemy animations

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour {
     public float lookRadius = 10f;
     public Transform target;
     NavMeshAgent agent;
+    public Animator anim;
+    public float controlSpeed;
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -15,16 +17,25 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        anim.SetFloat("speed", controlSpeed);
+        controlSpeed = 0f;
+        agent.enabled = true;
+
         float distance = Vector3.Distance(target.position, transform.position);
         if(distance <= lookRadius)
         {
+            controlSpeed = 1f;
+
             agent.SetDestination(target.position);
             if(distance <= agent.stoppingDistance)
             {
                 FaceTarget();
+                controlSpeed = 2f;
+                agent.enabled = false;
             }
         }
-	}
+
+    }
     void FaceTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
