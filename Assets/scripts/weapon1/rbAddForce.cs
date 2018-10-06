210fa563fa417f35ff4b1060e70369c43f19f007
Legendary888 <dennistodorov88@gmail.com>
2018-10-07 00:52:02 +0300

    rocketlauncher added

---
+++ Assets/scripts/weapon1/rbAddForce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rbAddForce : MonoBehaviour {
+    Rigidbody rb;
+    public float thrust = 1000;
+    Transform barrel;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        barrel = transform.Find("rocketbarrel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(transform.forward * thrust);
+        
+	}
+}
---
+++ Assets/scripts/weapon1/weaponRocket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponRocket : MonoBehaviour {
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+           // Rigidbody rocketInstance;
+            Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation);
+            rocketPrefab.AddForce(barrelEnd.forward * 5000);
+        }
+	}
+}
