fd0d36b17dbd10fb5eaf19d94d9ae4cb07576b79
Legendary888 <dennistodorov88@gmail.com>
2018-10-08 01:17:47 +0300

    vendor added

---
+++ Assets/scripts/npcShop1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcShop1 : MonoBehaviour {
+
+    public GameObject ui1;
+    public GameObject ui2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            openVendor();
+        }
+	}
+    public void openVendor()
+    {
+        ui2.SetActive(true);
+
+        
+    }
+    public void closeVendor()
+    {
+        ui2.SetActive(false);
+
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ui1.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ui1.SetActive(false);
+            ui2.SetActive(false);
+            closeVendor();
+        }
+    }
+}
