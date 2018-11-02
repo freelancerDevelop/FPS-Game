5cbbc89af01842f605d830c8733368c48eacce0b
Legendary888 <dennistodorov88@gmail.com>
2018-11-02 12:49:16 +0200

    added dropping weapons

---
+++ Assets/scripts/itemsPickUp/dropPickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class dropPickUp : MonoBehaviour {
+
+    public GameObject dropItem;
+    public Transform dropTransform;
+    public UnityEvent ondisabled;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnDisable()
+    {
+        dropPickup();
+    }
+    public void dropPickup()
+    {
+        Instantiate(dropItem, dropTransform.transform.position, dropTransform.transform.rotation);
+    }
+}
