68756fb8182541d086069005a6c67218d4311d5d
Legendary888 <dennistodorov88@gmail.com>
2018-11-15 01:11:25 +0200

    improved level 3

---
+++ Assets/scripts/player2/jump1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump1 : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+    public float thrust;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerJump();
+            Debug.Log("on trigger enter");
+        }
+    }
+    void playerJump()
+    {
+        rigidbody.AddForce(0, 0, thrust, ForceMode.Impulse);
+    }
+}
---
+++ Assets/scripts/player2/level2/enableOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableOnClick : MonoBehaviour {
+
+    public GameObject platform1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "cube1")
+        {
+            platform1.SetActive(true);
+        }
+    }
+}
